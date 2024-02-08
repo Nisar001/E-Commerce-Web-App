d56919294ab10331f79210e6b8b0664c1235629b
Nisar Khan <khannisarnk0786@gmail.com>
2024-02-08 13:17:13 +0530

    token saving in local storage

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,26 +1,41 @@
 import React, {useState} from 'react'
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 import Layout from '../../components/layout/Layout'
-import { toast } from 'react-toastify'
+import toast from 'react-hot-toast';
+import '../../styles/AuthStyles.css';
+
 const Register = () => {
    const [name, setName] = useState("")
    const [email, setEmail] = useState("")
    const [password, setPassword] = useState("")
    const [phone, setPhone] = useState("")
    const [address, setAddress] = useState("")
-
+   const navigate = useNavigate();
    // form submit function
 
-   const handleSubmit = (e) => {
+   const handleSubmit = async (e) => {
       e.preventDefault();
-      console.log(name, email, password, phone, address);
-      toast.success("Registration sucessfully")
-   }
 
+      try {
+         const res = await axios.post('/api/v1/auth/register', {name, email, password, phone, address});
+         if(res && res.data.success){
+            toast.success(res.data && res.data.message);
+            navigate('/login')
+         }else{
+            toast.error(res.data.message)
+         }
+
+      } catch (error) {
+         console.log(error);
+         toast.error('Something went wrong');
+      }
+   }
    return (
     <Layout title={'Sign-Up'}>
-      <div className="register">
-         <h1>Register Here</h1>
+      <div className="form-container">
          <form onSubmit={handleSubmit}>
+            <h4 className='title'>Register Here</h4>
             <div className="mb-3">
                <input type="text" onChange={(e) => setName(e.target.value)} value={name} className="form-control" id="exampleInputEmail1" placeholder='Enter Your Name' required/>
             </div>
